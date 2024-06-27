58cd502c3017ebc0b5d1d82844fe2cc35217b9b8
DESKTOP-F94NA43\Carlos <carloscaamal3@gmail.com>
2024-06-26 19:29:45 -0600

    particulas

---
+++ script.js
@@ -0,0 +1,160 @@
+$(document).ready(function() {
+  
+  setInterval(function() {
+  
+
+    var nextItem = productCurrentItem.next();
+
+    productCurrentItem.removeClass('active');
+
+    if (nextItem.length) {
+
+      productCurrentItem = nextItem.addClass('active');
+    } else {
+      productCurrentItem = productItem.first().addClass('active');
+    }
+
+    calcProductHeight();
+    animateContentColor();
+  
+  }, 5000); //Dura 3 secs cada imagen
+  
+  
+  
+  var getProductHeight = $('.product.active').height();
+
+  $('.products').css({
+    height: getProductHeight
+  });
+
+  function calcProductHeight() {
+    getProductHeight = $('.product.active').height();
+
+    $('.products').css({
+      height: getProductHeight
+    });
+  }
+
+  function animateContentColor() {
+    var getProductColor = $('.product.active').attr('product-color');
+
+  }
+
+  var productItem = $('.product'),
+    productCurrentItem = productItem.filter('.active');
+  
+});
+
+
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": ["#BD10E0","#B8E986","#50E3C2","#FFD300","#E86363"]
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 0.2,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
